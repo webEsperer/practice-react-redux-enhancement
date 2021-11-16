359eed70cd14faef2da4a8169146089e1fef2186
Mateusz Bogolubow <mateusz@bogolubow.com>
2021-11-16 12:19:09 +0100

    Add link to GH API doc

--- src/modules/github/github.api.js
+++ src/modules/github/github.api.js
@@ -2,7 +2,8 @@ class GitHubAPI {
     url = 'https://api.github.com/';
 
     getRepos(userName) {
-        return fetch(`${this.url}${userName}/repos/`)
+        // https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
+        return fetch(`${this.url}/users/${userName}/repos`)
             .then(this.handleErrors)
             .then(resp => resp.json())
     }
